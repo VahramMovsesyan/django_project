aadbee890b580a9b04193fb02aa44d529b7eefde
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-07-29 16:57:44 +0400

    rendering templates

--- challenges/views.py
+++ challenges/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
-from django.template.loader import render_to_string
+# from django.template.loader import render_to_string
 
 months_info = {
     "january": "Eat no meat for the entire month!",
@@ -49,7 +49,8 @@ def monthly_challenges_by_numbers(request, month_as_number):
 def monthly_challenges(request, month):
     try:
         challeng_text = months_info[month]
-        response_data = render_to_string("challenges/challenge.html")
-        return HttpResponse(response_data)
+        return render(request,"challenges/challenge.html")  # we can use this insted of two under lines 
+        # response_data = render_to_string("challenges/challenge.html")
+        # return HttpResponse(response_data)
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
