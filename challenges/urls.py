a2a09551cada0248c8cdff54ba7f9dcc6001c5ae
Vahram Movsesyan <vahrammovsesyan333@gmail.com>
2022-07-30 02:46:47 +0400

    using INCLUDE tag and adding it in challenge.html and index.html

--- challenges/urls.py
+++ challenges/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path("<int:month_as_number>", views.monthly_challenges_by_numbers),
     path("<str:month>", views.monthly_challenges, name="month-challenge"),
-    path("", views.index)  # challenges/
+    path("", views.index, name="index")  # challenges/
 ]
